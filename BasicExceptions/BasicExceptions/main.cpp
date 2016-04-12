c4f14302dd1f9360097ce20d9d45db3de4bd5285
AndrewM <amorales2@live.com>
2016-04-11 23:10:54 -0500

    added main.cpp to BasicExceptions as well as sample code from lecture

--- BasicExceptions/BasicExceptions/main.cpp
+++ BasicExceptions/BasicExceptions/main.cpp
@@ -1,6 +1,56 @@
+///////////////////////////////////////////////////////////////////////////////
+//main.cpp
+//
+
 #include <iostream>
+#include <string>
+
+void mightGoWrong()
+{
+	bool error1 = false;
+	bool error2 = true;
+
+	if (error1)
+	{
+		throw "something went wrong";
+	}
+	if (error2)
+	{
+		throw std::string("Something else went wrong");
+	}
+}
+
+void usesMightGoWrong()
+{
+	mightGoWrong();
+}
 
 int main()
 {
+	try
+	{
+		usesMightGoWrong();
+	}
+	catch (int e)
+	{
+		switch (e)
+		{
+		case 8:
+			std::cout << "Error code: " << e << std::endl;
+		default:
+			break;
+		}
+	}
+	catch (char const* e)
+	{
+		std::cout << "Error Message: " << e << std::endl;
+	}
+	catch (std::string e)
+	{
+		std::cout << "Error Message: " << e << std::endl;
+	}
+
+	std::cout << "Still working!";
+
 	return 0;
 }
