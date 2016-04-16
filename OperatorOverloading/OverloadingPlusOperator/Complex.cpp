63a313d52f2fc2ac738192286ec4fa6435b56b48
AndrewM <amorales2@live.com>
2016-04-16 13:51:08 -0500

    added OverloadingPlusOperator project

---
+++ OperatorOverloading/OverloadingPlusOperator/Complex.cpp
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////
+//Complex.cpp
+//
+#include "Complex.h"
+
+Complex::Complex() :
+	real(0),
+	imaginary(0)
+{
+
+}
+
+Complex::Complex(double real, double imaginary) :
+	real(real),
+	imaginary(imaginary)
+{
+
+}
+
+Complex::Complex(const Complex & other)
+{
+	real = other.real;
+	imaginary = other.imaginary;
+}
+
+const Complex & Complex::operator=(const Complex & other)
+{
+	real = other.real;
+	imaginary = other.imaginary;
+	return *this;
+}
+
+std::ostream & operator<<(std::ostream & out, const Complex & c)
+{
+	out << "(" << c.getReal() << "," << c.getImagionary() << ")";
+	return out;
+}
+
+Complex operator+(const Complex & c1, const Complex & c2)
+{
+	return Complex(c1.getReal()+c2.getReal(), c1.getImagionary()+c2.getImagionary());
+}
+
+Complex operator+(const Complex & c1, double d)
+{
+	return Complex(c1.getReal()+d , c1.getImagionary());
+}
+
+Complex operator+(double d, const Complex & c1)
+{
+	return Complex(c1.getReal() + d, c1.getImagionary());
+}
