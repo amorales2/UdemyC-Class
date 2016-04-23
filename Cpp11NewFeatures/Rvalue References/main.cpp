0fb99529c5301e578d1b2d6a69990b20ca7d2797
AndrewM <amorales2@live.com>
2016-04-23 14:56:15 -0500

    added "Move Constructors" project

--- Cpp11NewFeatures/Rvalue References/main.cpp
+++ Cpp11NewFeatures/Rvalue References/main.cpp
@@ -16,14 +16,14 @@ private:
 public:
 	Test()
 	{
-		std::cout << "Constructor" << std::endl;
+		//std::cout << "Constructor" << std::endl;
 		//added curly braces to zero all the bytes
 		m_pBuffer = new int[SIZE] {};
 	}
 
 	Test(int i)
 	{
-		std::cout << "Parameterized constructor" << std::endl;
+		//std::cout << "Parameterized constructor" << std::endl;
 
 		for (int i = 0;i < SIZE;++i)
 		{
@@ -33,7 +33,7 @@ public:
 
 	Test(const Test &other)
 	{
-		std::cout << "copy constructor" << std::endl;
+		//std::cout << "copy constructor" << std::endl;
 		m_pBuffer = new int[SIZE] {};
 
 		memcpy(m_pBuffer, other.m_pBuffer, SIZE*sizeof(int));
@@ -42,13 +42,13 @@ public:
 
 	Test &operator = (const Test &other)
 	{
-		std::cout << "assignment" << std::endl;
+		//std::cout << "assignment" << std::endl;
 		return *this;
 	}
 
 	~Test()
 	{
-		std::cout << "destructor" << std::endl;
+		//std::cout << "destructor" << std::endl;
 		//use delete with brackets for arrays
 		delete[] m_pBuffer;
 	}
@@ -67,6 +67,16 @@ Test getTest()
 	return Test();
 }
 
+void check(const Test &value)
+{
+	std::cout << "lValue function!" << std::endl;
+}
+
+void check(const Test &&value)
+{
+	std::cout << "rValue function!" << std::endl;
+}
+
 int main()
 {
 	Test test1 = getTest();
@@ -74,6 +84,15 @@ int main()
 
 	std::vector<Test> vec;
 	vec.push_back(Test());
+
+	//double reference makes this an rValue reference.
+	Test &lTest1 = test1;
+	Test &&rTest1 = Test();
+	Test &&rTest2 = getTest();
+	check(test1);		//lvalue overload
+	check(getTest());	//rvalue overload
+	check(Test());		//rvalue overload
+
 
 	//know the difference between lvalues and rvalues
 	//and lvalue is anything you can take the address of.
