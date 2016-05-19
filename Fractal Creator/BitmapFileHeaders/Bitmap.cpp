758f10901d8a5c2d267f295786aa2c62aa8626c1
AndrewM <amorales2@live.com>
2016-05-18 23:23:33 -0500

    completed lecture 76 by adding the bitmap headers to the bitmap class

--- Fractal Creator/BitmapFileHeaders/Bitmap.cpp
+++ Fractal Creator/BitmapFileHeaders/Bitmap.cpp
@@ -2,11 +2,13 @@
 //Bitmap.cpp
 //
 #include "Bitmap.h"
+#include "BitmapInfoHeader.h"
+#include "BitmapFileHeader.h"
 
 Bitmap::Bitmap(int width, int height) :
 	m_width(width),
 	m_height(height),
-	m_pPixels(new uchar[m_width * m_height * 3])
+	m_pPixels(new uchar[m_width * m_height * 3]{})
 {
 
 }
@@ -18,6 +20,15 @@ void Bitmap::setPixel(int x, int y, uchar red, uchar green, uchar blue)
 
 bool Bitmap::write(std::string filename)
 {
+	BitmapFileHeader fileHeader;
+	BitmapInfoHeader infoHeader;
+
+	fileHeader.fileSize = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader) + m_width*m_height * 3;
+	fileHeader.dataOffset = sizeof(BitmapFileHeader) + sizeof(BitmapInfoHeader);
+
+	infoHeader.width = m_width;
+	infoHeader.height = m_height;
+
 	//TODO
 	return false;
 }
